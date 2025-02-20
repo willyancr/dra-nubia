769441fdb6c3737be7c707f4e06eb98488cc1950
Willyan Costa <willyancr@gmail.com>
2025-02-19 21:29:13 -0300

    feat: Adicionar metadados estruturados para a Dra. Núbia Carmo na página

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import ButtonBackToTop from "@/components/button-back-to-top";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,6 +25,42 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "MedicalOrganization",
+              name: "Dra. Núbia Carmo – Reumatologista Pediátrica",
+              url: "https://www.dranubiareumatopediatra.com.br",
+              sameAs: ["https://www.instagram.com/dranubiareumatopediatra"],
+              address: {
+                "@type": "PostalAddress",
+                streetAddress:
+                  "EndUrban Futuro - Q. 201 Sul Avenida Joaquim Teotônio Segurado, 15 - Sala 301 - Plano Diretor Sul",
+                addressLocality: "Palmas",
+                addressRegion: "TO",
+                postalCode: "77015-200",
+              },
+              medicalSpecialty: "Reumatologia Pediátrica",
+              physician: {
+                "@type": "Physician",
+                name: "Dra. Núbia Carmo",
+                medicalSpecialty: "Reumatologia Pediátrica",
+                address: {
+                  "@type": "PostalAddress",
+                  streetAddress:
+                    "EndUrban Futuro - Q. 201 Sul Avenida Joaquim Teotônio Segurado, 15 - Sala 301 - Plano Diretor Sul",
+                  addressLocality: "Palmas",
+                  addressRegion: "TO",
+                  postalCode: "77015-200",
+                },
+              },
+            }),
+          }}
+        />
+      </Head>
       <body
         className={`${poppins.className} mx-auto max-w-[1200px] bg-[#A7D1E6]/15 antialiased sm:px-6`}
       >
